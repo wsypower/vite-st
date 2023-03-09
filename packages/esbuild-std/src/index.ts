892aabfa05af072ddde0bb5579bb7af32f68e6a7
weiyafei <2553241022@qq.com>
2023-03-09 13:13:07 +0800

    esbuild --build

--- packages/esbuild-std/src/index.ts
+++ packages/esbuild-std/src/index.ts
@@ -2,14 +2,15 @@
  * @Description:
  * @Author: wsy
  * @Date: 2023-03-09 02:18:36
- * @LastEditTime: 2023-03-09 02:21:48
+ * @LastEditTime: 2023-03-09 13:10:58
  * @LastEditors: wsy
  */
-import { reactive } from 'vue'
 
 interface IState {
   count: number
   name: string
 }
 
-const test: IState = reactive({ name: 'wsy', count: 0 })
+const test = { name: 'wsy', count: 0 }
+
+export default test
